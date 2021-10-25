c6e3c7fe7db8c7c07027c03449f6df6b3542ef65
ChristianH545 <88940167+ChristianH545@users.noreply.github.com>
2021-10-25 20:18:19 -0300

    arreglo de error

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let nombreUsuario = prompt("Ingrese Su Nombre. POR FAVOR!");
 let apellido = prompt("Ingrese su Apellido. POR FAVOR!");
 
-alert("Hola: " + nombreUsuario + ' ' + apellido); 
+alert("Hola: " + nombreUsuario + " " + apellido);
 
 let a = 75;
-let b = parseInt( prompt(" Ingrese el valor de B"));
-a = 75 + b;
-console.log(a + b );
+let b = parseInt(prompt(" Ingrese el valor de B"));
+
+console.log("Valor de b = " + b + " valor de a = " + a);
+
+let total = a + b;
+console.log(" El Resultado de este Algoritmo  a + b = " + total);
