bab70e90c5b53aa5c423f001ed577b8471221047
ChristianH545 <88940167+ChristianH545@users.noreply.github.com>
2021-10-24 23:37:28 -0300

    desafio 1 javascrip

---
+++ app.js
@@ -0,0 +1,9 @@
+let nombreUsuario = prompt("Ingrese Su Nombre. POR FAVOR!");
+let apellido = prompt("Ingrese su Apellido. POR FAVOR!");
+
+alert("Hola: " + nombreUsuario + ' ' + apellido); 
+
+let a = 75;
+let b = parseInt( prompt(" Ingrese el valor de B"));
+a = 75 + b;
+console.log(a + b );
