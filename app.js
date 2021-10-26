0960c88329f735bc9157129d7d55a412080e4e1b
ChristianH545 <88940167+ChristianH545@users.noreply.github.com>
2021-10-26 20:41:56 -0300

    Desafio 1 Javascript

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ let apellido = prompt("Ingrese su Apellido. POR FAVOR!");
 alert("Hola: " + nombreUsuario + " " + apellido);
 
 let a = 75;
-let b = parseInt(prompt(" Ingrese el valor de B"));
+let b = parseFloat(prompt(" Ingrese el valor de B"));
 
 console.log("Valor de b = " + b + " valor de a = " + a);
 
 let total = a + b;
 console.log(" El Resultado de este Algoritmo  a + b = " + total);
+``
